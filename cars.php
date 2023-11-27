16e0de28650025c20421ac7f182ed71024a28faa
3041TAN-18 <3041TAN-18@boronkay.local>
2023-11-27 11:15:54 +0100

    makers

--- cars.php
+++ cars.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('memory_limit','-1');
 $array = [];
-$result = [];
+
 $file = "car-db.csv";
 function getCsvData($file){
     if(!file_exists($file)){
@@ -21,31 +21,37 @@ $csvData = getCsvData($file);
 //print_r($csvData);
 
 //$sv = count($csvData);
-$header = $csvData[0];
-$makerKey = array_search('make',$header);
-$modelKey = array_search('model',$header);
+function getMakers(){
+    $header = $csvData[0];
+    $makerKey = array_search('make',$header);
+    //$modelKey = array_search('model',$header);
 
-$maker ="";
-$model = "";
-$isHeader = true;
-$makers = [];
+    $maker ="";
+    //$model = "";
+    $isHeader = true;
+    $makers = [];
+    //$result = [];
 
-foreach ($csvData as $data) {
-    if (!is_array($data)){
-        continue;
+    foreach ($csvData as $data) {
+        if (!is_array($data)){
+            continue;
+        }
+        if ($isHeader) {
+            $isHeader = false;
+            continue;
+        }
+        if ($maker != $data[$makerKey]) {
+            $maker = $data[$makerKey];
+            $makers[] = $maker;
+        }
+        /*if ($model != $data[$modelKey]) {
+            $model = $data[$modelKey ];
+            $result[$maker][] = $model;
+        }*/
     }
-    if ($isHeader) {
-        $isHeader = false;
-        continue;
-    }
-    if ($maker != $data[$makerKey]) {
-        $maker = $data[$makerKey];
-        $makers[] = $maker;
-    }
-    if ($model != $data[$modelKey]) {
-        $model = $data[$modelKey ];
-        $result[$maker][] = $model;
-    }
+    return $makers;
 }
 print_r($makers);
+
+$mysqli = new mysqli;
 ?>
