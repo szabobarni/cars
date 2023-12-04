2c8f8b22c23d24031e59c1d14224f102c9a36a81
3041TAN-18 <3041TAN-18@boronkay.local>
2023-12-04 10:20:10 +0100

    gutyuli

--- db-tools-class.php
+++ db-tools-class.php
@@ -16,12 +16,49 @@ const DBTABLE = 'makers';
     }
     function insertMakers($maker, $truncate = false){
         if($truncate) {
-            $mysqli->query("TRUNCATE TABLE makers");
+            $this->mysqli->query("TRUNCATE TABLE makers");
         }
-        $result = $mysqli->query("INSERT INTO makers (name) VALUES ('$maker')");
+        $result = $this->mysqli->query("INSERT INTO makers (name) VALUES ('$maker')");
         if (!$result){
             echo "Hiba történt a $maker beszúrása közben";
         }
         return $result;
+    }
+    function updateMaker($data){
+        $makerName = $data['name'];
+    
+        $result = $this->mysqli->query("UPDATE makers SET name=$makerName");
+    
+        if (!$result){
+            echo "Hiba történt a $makerName beszúrása közben";
+        }
+        $maker = getMakerByName($makerName);
+        return $result;
+    }
+    
+    function getMaker($id){
+        $result = $this->mysqli->query("SELECT * FROM makers WHERE id=$id");
+        $maker = $result->fetch_assoc();
+    
+        return $maker;
+    }
+    
+    function getMakerByName($name){
+        $result = $this->mysqli->query("SELECT * FROM makers WHERE name=$name");
+        $maker = $result->fetch_assoc();
+    
+        return $maker;
+    }
+    
+    function delMaker($id){
+        $result = $this->mysqli->query("DELETE makers WHERE id=$id");
+        return $result;
+    }
+    
+    function getAllMakers(){
+        $result = $this->mysqli->query("SELECT * FROM makers");
+        $makers = $result->fetch_assoc();
+    
+        return $makers;
     }
 }
