6e783ec756a4c5eae13f63e7c439268a8b7c4572
3041TAN-18 <3041TAN-18@boronkay.local>
2023-12-04 08:30:33 +0100

    ijk

--- cars.php
+++ cars.php
@@ -1,38 +1,70 @@
 <?php
 ini_set('memory_limit','-1');
 $array = [];
-$result = [];
+
 $file = "car-db.csv";
 function getCsvData($file){
-if(!file_exists($file)){
-    echo "$file nem található";
-    return false;
-}
-
-if(file_exists($file)) {
+    if(!file_exists($file)){
+        echo "$file nem található";
+        return false;
+    }
     $csv = fopen($file, 'r');
     while (!feof($csv)) {
         $line = fgetcsv($csv);
         $array[] = $line;
-      }
-       fclose($csv);
     }
-    return $array ;
+    fclose($csv);
+    return $array ;    
 }
 
 $csvData = getCsvData($file);
 //print_r($csvData);
 
-$sv = count($csvData);
-$maker ="";
-$model = "";
-for ($i=0; $i < $sv; $i++) { 
-    if ($maker != $csvData[$i][1]) {
-        $maker = $csvData[$i][1];
+//$sv = count($csvData);
+function getMakers($csvData){
+    $header = $csvData[0];
+    $makerKey = array_search('make',$header);
+    //$modelKey = array_search('model',$header);
+
+    $maker ="";
+    //$model = "";
+    $isHeader = true;
+    $makers = [];
+    //$result = [];
+
+    foreach ($csvData as $data) {
+        if (!is_array($data)){
+            continue;
+        }
+        if ($isHeader) {
+            $isHeader = false;
+            continue;
+        }
+        if ($maker != $data[$makerKey]) {
+            $maker = $data[$makerKey];
+            $makers[] = $maker;
+        }
+        /*
+        if ($model != $data[$modelKey]) {
+            $model = $data[$modelKey ];
+            $result[$maker][] = $model;
+        }
+        */
     }
-    if ($model != $csvData[$i][2]) {
-        $result[$maker][] = $model;
-    }
+    return $makers;
 }
-print_r($result);
-?>
+
+$mysqli = new mysqli("localhost","root",null,"cars");
+
+//Check connection
+if($mysqli->connect_errno) {
+    echo "Failed to connect to MySql: " . $mysqli -> connect_error;
+    exit();
+}
+echo "connected\n";
+
+$makers = getMakers($csvData);
+
+$result = insertMarkes($mysqli, $makers, true);
+
+$mysqli->close();
